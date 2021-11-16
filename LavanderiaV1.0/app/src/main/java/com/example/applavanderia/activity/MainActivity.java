8bf885a38ac035f7979ed83955d7128402d049f4
Victor <victor-laranjeira18@hotmail.com>
2021-11-16 17:56:24 -0300

    Commit inivial de appLavanderia

--- LavanderiaV1.0/app/src/main/java/com/example/applavanderia/activity/MainActivity.java
+++ LavanderiaV1.0/app/src/main/java/com/example/applavanderia/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.applavanderia;
+package com.example.applavanderia.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.applavanderia.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ LavanderiaV1.0/app/src/main/java/com/example/applavanderia/activity/ValidadorActivity.java
@@ -0,0 +1,16 @@
+package com.example.applavanderia.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.applavanderia.R;
+
+public class ValidadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validador);
+    }
+}
